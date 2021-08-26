b95fe68bf7dde1a60e67976d053249d715f0989b
Is3n <isen0607@gmail.com>
2021-08-26 16:31:50 +0200

    Eureka Server OK

---
+++ EurekaServer/src/test/java/hepl/isen/EurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hepl.isen.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
