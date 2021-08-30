c376433c0568fdf2327c6ce9245e71b1b74b2a97
Is3n <isen0607@gmail.com>
2021-08-30 12:36:22 +0200

    Requirements added

--- MainApp/src/main/java/hepl/isen/MainApp/fournisseur/FournisseurService.java
+++ MainApp/src/main/java/hepl/isen/MainApp/fournisseur/FournisseurService.java
@@ -18,7 +18,7 @@ public class FournisseurService {
         List<Fournisseur> fournisseurList = new LinkedList<Fournisseur>();
 
         boolean dispo;
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 2; i++) {
 
             dispo = true;
             int randomWithMathRandom = (int) ((Math.random() * (100 - 1)) + 1);
